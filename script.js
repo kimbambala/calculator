6c4e16b51f9bfbb17c691a9b54337815ad1a909d
Nick <nickprincy@gmail.com>
2023-02-09 16:51:33 -0800

    functions for operators are created

--- script.js
+++ script.js
@@ -3,4 +3,28 @@ const operators = document.querySelectorAll(".operator");
 const decimal= document.querySelector(".decimal");
 const clear= document.querySelector(".clear");
 const screen = document.querySelector(".screen");
+const equal = document.querySelector(".equal")
 
+
+function add (){
+
+}
+
+function subtract (){
+
+}
+
+
+function multiply (){
+
+}
+
+
+function divide (){
+
+}
+
+
+function operate(){
+    
+}
